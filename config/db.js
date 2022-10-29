dc7709738652c5a1100fc2fa642b08601c45303f
Ramesh Banakar <ramesh.s.b@campusuvce.in>
2022-10-29 14:42:55 +0530

    adding user into db

---
+++ config/db.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connetDB = async () => {
+  try {
+    await mongoose.connect(db, { useNewUrlParser: true });
+    console.log("mongodb conneted....");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+  //   mongoose
+  //      .connect(db, {
+  //         useNewUrlParser: true,
+  //         //useCreateIndex: true,
+  //     //   useFindAndModify: false,
+  //      }
+  //     )
+  //     .then(() => console.log("mongoDB conneted"))
+  //     .catch((err) => {
+  //       console.log(err.message);
+  //       process.exit(1);
+  //     });
+};
+module.exports = connetDB;
--- router/users.js
+++ router/users.js
@@ -1,6 +1,33 @@
-const express=require("express")
-const router=express.Router()
+const express = require("express");
+const router = express.Router();
+const bcrypt=require("bcryptjs")
+const {check,validationResult}=require("express-validator/check");
+const User = require("../models/User");
+router.post("/", [
+   check("name","enter the valid name").isLength({min:3}),
+   check("email","enter the valid mail id").isEmail(),
+   check("password","Enter the valid password").isLength({min:5})
 
-router.post("/",(req,res)=>{res.send("hellow users")})
+],async(req, res) => {
+  const error=validationResult(req)
+  if(!error.isEmpty()){
+    return res.status(400).json({errors:error.array()})
+  }
+  let {name,email,password}=req.body
+  try {
+    let user=await User.findOne({email})
+    if(user){
+      return res.status(400).json({msg:"user already exists"})
+    }
+    user=new User({name,email,password})
+    let salt=await bcrypt.genSalt(10)
+    user.password=await bcrypt.hash(password,salt)
+    await user.save()
+    res.send("user saved successfully")
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).send("server error")
+  }
+});
 
-module.exports=router
+module.exports = router;
