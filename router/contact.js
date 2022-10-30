232ea83c0f91d6cdc2fc7966ef4a6f2ee3804748
Ramesh Banakar <ramesh.s.b@campusuvce.in>
2022-10-30 10:59:03 +0530

    contact update

--- router/contact.js
+++ router/contact.js
@@ -1,48 +1,68 @@
 const express = require("express");
 const router = express.Router();
-const {check,validationResult}=require("express-validator/check")
-const auth=require("../middleware/auth")
-const Contact=require("../models/contact")
+const { check, validationResult } = require("express-validator/check");
+const auth = require("../middleware/auth");
+const Contact = require("../models/contact");
 
-router.get("/",auth,async (req, res) => {
+router.get("/", auth, async (req, res) => {
   try {
-    const contacts=await Contact.find({user:req.user.id}).sort("name")
-    res.json(contacts)
+    const contacts = await Contact.find({ user: req.user.id }).sort("name");
+    res.json(contacts);
   } catch (error) {
-    console.error(error.message)
-    res.status(500).send("server error")
+    console.error(error.message);
+    res.status(500).send("server error");
   }
 });
 
-router.post("/",[auth,[
-    check("name","Enter the valid name").not().isEmpty()
-]], async(req, res) => {
-    const error=validationResult(req)
-    if(!error.isEmpty()){
-        return res.status(400).send({errors:error.array()})
+router.post(
+  "/",
+  [auth, [check("name", "Enter the valid name").not().isEmpty()]],
+  async (req, res) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      return res.status(400).send({ errors: error.array() });
     }
-    const {name,email,phone,type}=req.body
+    const { name, email, phone, type } = req.body;
     try {
-        const contact=new Contact({name,email,phone,type,user:req.user.id})
-        
-        await contact.save()
-        res.json(contact)
-        
+      const contact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id,
+      });
+
+      await contact.save();
+      res.json(contact);
     } catch (error) {
-        console.log(error.message)
-        res.status(500).send("server error")
+      console.log(error.message);
+      res.status(500).send("server error");
     }
-    
+  }
+);
 
-});
+router.put("/:id", auth,async (req, res) => {
+  const { name, email, phone } = req.body;
+  const contactField = {};
+  if (name) contactField.name = name;
+  if (email) contactField.email = email;
+  if (phone) contactField.phone = phone;
 
-router.put("/:id", (req, res) => {
-    res.send("put contact");
+  try {
+    let contact=await Contact.findById(req.params.id)
+    if(!contact) return res.status(404).json({msg:"user not found"})
+    if(contact.user.toString()!==req.user.id){
+       return res.status(404).send("unauthorized to edit")
+    }
+    contact=await Contact.findByIdAndUpdate(req.params.id,{$set:contactField},{new:true})
+    res.send(contact)
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).send("server error")
+  }
 });
 router.delete("/", (req, res) => {
-    res.send("delete contact");
+  res.send("delete contact");
 });
-
-
 
 module.exports = router;
